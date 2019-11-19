9b5537ad98cf6e34fa884dad9aad5e0489842dc9
ibrahim dursun <ibrahim.dursun@gmail.com>
2019-11-19 13:03:00 +0000

    Add some mechanism around sidebar

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, a, aside, div, h1, i, img, input, li, nav, span, text, ul)
-import Html.Attributes exposing (class, placeholder, src, type_)
+import Html.Attributes exposing (class, placeholder, src, type_, href)
 
 
 
@@ -15,7 +15,9 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( 
+        
+        {}, Cmd.none )
 
 
 
@@ -41,51 +43,58 @@ navbar =
         [ div [ class "flex-initial w-1/2 mx-auto " ]
             [ input [ class "justify-center w-full border p-2 rounded", placeholder "Search courses", type_ "text" ]
                 []
-            , text "  "
+               , text "  "
             ]
         , div [ class "flex items-center" ]
             []
         ]
 
+type alias SidebarItem = 
+    {
+        name: String
+    ,   link: String
+    }
 
 sidebar : Html Msg
 sidebar =
+
     aside [ class "flex-none w-full h-full border-r shadow-lg h-screen bg-gray-800 " ]
         [ h1 [ class "h-24 text-white tracking-4 uppercase font-bold p-4 text-left text-middle leading-lg text-lg" ]
             [ text "ONAT'S ACADEMY" ]
-        , sidebarSection "Repositories"
+        , sidebarSection "Repositories" [{name= "Item1", link="#"}, {name="Item2", link="#"}] False
+        , sidebarSection "Files"  [{name= "Item1", link="#"}, {name="Item2", link="#"}] True 
+        , sidebarSection "Services"  [{name= "Item1", link="#"}, {name="Item2", link="#"}] False
         ]
 
 
-sidebarSectionHeader : String -> Html Msg
-sidebarSectionHeader header =
+sidebarSectionHeader : String -> Bool-> Html Msg
+sidebarSectionHeader header collapsed =
+    let 
+        chevronStyle = if collapsed then "flex-none fa fa-chevron-right" else "flex-none fa fa-chevron-down"
+    in
     div [ class "flex items-center font-bold text-gray-500 uppercase pb-2 px-2" ]
         [ i [ class "flex-none" ]
             []
         , span [ class "flex-grow mx-1 select-none" ]
             [ text header ]
-        , i [ class "flex-none" ]
+        , i [ class chevronStyle]
             []
         , text ""
         ]
 
 
-sidebarSectionItem : String -> Html Msg
-sidebarSectionItem title =
+sidebarSectionItem : SidebarItem -> Html Msg
+sidebarSectionItem {name, link} =
     li []
-        [ a [ class "block py-2 text-sm pl-5 text-gray-500 select-none hover:bg-gray-900 hover:shadow-lg hover:text-gray-300" ] [ text title ]
+        [ a [ class "block py-2 text-sm pl-5 text-gray-500 select-none hover:bg-gray-900 hover:shadow-lg hover:text-gray-300", href link ] [ text name ]
         ]
 
 
-sidebarSection : String -> Html Msg
-sidebarSection header =
+sidebarSection : String -> List SidebarItem -> Bool -> Html Msg
+sidebarSection header items collapsed =
     div [ class "m-2 my-3" ]
-        [ sidebarSectionHeader header
-        , ul [ class "list-reset" ]
-            [ li []
-                [ a [ class "block py-2 text-sm pl-5 text-gray-500 select-none hover:bg-gray-900 hover:shadow-lg hover:text-gray-300" ] [ text "Item1" ]
-                ]
-            ]
+        [ sidebarSectionHeader header collapsed
+        , ul [ class "list-reset" ] (if collapsed then [] else (List.map sidebarSectionItem items))
         ]
 
 
