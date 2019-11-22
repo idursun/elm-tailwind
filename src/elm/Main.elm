71a4c88f65f07f60c1d3af79ab19632f78160c66
ibrahim dursun <ibrahim.dursun@gmail.com>
2019-11-22 11:00:27 +0000

    Use model during initialisation

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, a, aside, div, h1, i, img, input, li, nav, span, text, ul)
-import Html.Attributes exposing (class, placeholder, src, type_, href)
+import Html.Attributes exposing (class, href, placeholder, src, type_)
 
 
 
@@ -10,14 +10,32 @@ import Html.Attributes exposing (class, placeholder, src, type_, href)
 
 
 type alias Model =
-    {}
+    { sidebar : List SidebarModel
+    }
+
+
+type alias SidebarItem =
+    { name : String
+    , link : String
+    }
+
+
+type alias SidebarModel =
+    { name : String
+    , items : List SidebarItem
+    , collapsed : Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 
-        
-        {}, Cmd.none )
+    ( Model
+        [ SidebarModel "Repositories" [ SidebarItem "Item1" "#", SidebarItem "Item2" "#" ] False
+        , SidebarModel "Items" [ SidebarItem "Item1" "#", SidebarItem "Item2" "#" ] False
+        , SidebarModel "Services" [ SidebarItem "Item1" "#", SidebarItem "Item2" "#" ] False
+        ]
+    , Cmd.none
+    )
 
 
 
@@ -43,48 +61,45 @@ navbar =
         [ div [ class "flex-initial w-1/2 mx-auto " ]
             [ input [ class "justify-center w-full border p-2 rounded", placeholder "Search courses", type_ "text" ]
                 []
-               , text "  "
+            , text "  "
             ]
         , div [ class "flex items-center" ]
             []
         ]
 
-type alias SidebarItem = 
-    {
-        name: String
-    ,   link: String
-    }
 
-sidebar : Html Msg
-sidebar =
-
+sidebar : List SidebarModel -> Html Msg
+sidebar models =
     aside [ class "flex-none w-full h-full border-r shadow-lg h-screen bg-gray-800 " ]
         [ h1 [ class "h-24 text-white tracking-4 uppercase font-bold p-4 text-left text-middle leading-lg text-lg" ]
-            [ text "ONAT'S ACADEMY" ]
-        , sidebarSection "Repositories" [{name= "Item1", link="#"}, {name="Item2", link="#"}] False
-        , sidebarSection "Files"  [{name= "Item1", link="#"}, {name="Item2", link="#"}] True 
-        , sidebarSection "Services"  [{name= "Item1", link="#"}, {name="Item2", link="#"}] False
+            [ text "Some academy" ]
+        , h1 [] (models |> List.map (\x -> sidebarSection x.name x.items False))
         ]
 
 
-sidebarSectionHeader : String -> Bool-> Html Msg
+sidebarSectionHeader : String -> Bool -> Html Msg
 sidebarSectionHeader header collapsed =
-    let 
-        chevronStyle = if collapsed then "flex-none fa fa-chevron-right" else "flex-none fa fa-chevron-down"
+    let
+        chevronStyle =
+            if collapsed then
+                "flex-none fa fa-chevron-right"
+
+            else
+                "flex-none fa fa-chevron-down"
     in
     div [ class "flex items-center font-bold text-gray-500 uppercase pb-2 px-2" ]
         [ i [ class "flex-none" ]
             []
         , span [ class "flex-grow mx-1 select-none" ]
             [ text header ]
-        , i [ class chevronStyle]
+        , i [ class chevronStyle ]
             []
         , text ""
         ]
 
 
 sidebarSectionItem : SidebarItem -> Html Msg
-sidebarSectionItem {name, link} =
+sidebarSectionItem { name, link } =
     li []
         [ a [ class "block py-2 text-sm pl-5 text-gray-500 select-none hover:bg-gray-900 hover:shadow-lg hover:text-gray-300", href link ] [ text name ]
         ]
@@ -94,7 +109,13 @@ sidebarSection : String -> List SidebarItem -> Bool -> Html Msg
 sidebarSection header items collapsed =
     div [ class "m-2 my-3" ]
         [ sidebarSectionHeader header collapsed
-        , ul [ class "list-reset" ] (if collapsed then [] else (List.map sidebarSectionItem items))
+        , ul [ class "list-reset" ]
+            (if collapsed then
+                []
+
+             else
+                List.map sidebarSectionItem items
+            )
         ]
 
 
@@ -103,7 +124,7 @@ view model =
     div [ class "w-full" ]
         [ div [ class "flex" ]
             [ div [ class "flex-none w-64" ]
-                [ sidebar
+                [ sidebar model.sidebar
                 ]
             , div [ class "flex-grow" ]
                 [ navbar
