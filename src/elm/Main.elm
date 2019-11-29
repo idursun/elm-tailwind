45597b3966d8d9b28bbc0576c7edea4cadbd287a
ibrahim dursun <ibrahim.dursun@gmail.com>
2019-11-29 09:45:25 +0000

    Add a dodgy shopping cart total logic

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, input, nav, span, text)
+import Html exposing (Html, button, div, h1, input, nav, span, text)
 import Html.Attributes exposing (class, placeholder, type_)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 import Sidebar exposing (SidebarItem, SidebarModel, SidebarMsg, sidebar, updateSidebar)
 
 
@@ -22,10 +22,15 @@ type alias Course =
     }
 
 
+type alias NavbarModel =
+    { total : Float }
+
+
 type alias Model =
     { sidebars : List SidebarModel
     , courses : List Course
     , searchText : Maybe String
+    , navbar : NavbarModel
     }
 
 
@@ -45,6 +50,7 @@ init =
         , Course (CourseId 7) "Essentials 2" 200.0
         ]
         Nothing
+        { total = 0 }
     , Cmd.none
     )
 
@@ -60,6 +66,7 @@ type NavbarMsg
 type Msg
     = SidebarMsg SidebarMsg
     | NavbarMsg NavbarMsg
+    | BuyCourse CourseId
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -67,6 +74,13 @@ update msg model =
     case msg of
         SidebarMsg m ->
             ( { model | sidebars = List.map (updateSidebar m) model.sidebars }, Cmd.none )
+
+        BuyCourse id ->
+            let
+                total =
+                    List.filter (\x -> x.id == id) model.courses |> List.map .price |> List.sum
+            in
+            ( { model | navbar = { total = model.navbar.total + total } }, Cmd.none )
 
         NavbarMsg m ->
             case m of
@@ -78,16 +92,14 @@ update msg model =
 ---- VIEW ----
 
 
-navbar : Html Msg
-navbar =
+navbar : NavbarModel -> Html Msg
+navbar model =
     nav [ class "flex justify-between items-center bg-white m-0 p-2 shadow-lg" ]
-        [ div [ class "flex-initial w-1/2 mx-auto " ]
-            [ input [ class "justify-center w-full border p-2 rounded", placeholder "Search courses", type_ "text", onInput (SearchInput >> NavbarMsg) ]
-                []
-            , text "  "
+        [ div [ class "flex-initial w-1/2 mx-auto" ]
+            [ input [ class "justify-center w-full border p-2 rounded", placeholder "Search courses", type_ "text", onInput (SearchInput >> NavbarMsg) ] []
             ]
         , div [ class "flex items-center" ]
-            []
+            [ span [ class "tracking-wide" ] [ text "Total:" ], span [ class "pl-2 font-semibold" ] [ text <| String.fromFloat model.total, text "$" ] ]
         ]
 
 
@@ -108,9 +120,7 @@ view model =
                 [ Html.map SidebarMsg (sidebar model.sidebars)
                 ]
             , div [ class "flex-grow" ]
-                [ div [ class "flex-grow" ]
-                    [ navbar
-                    ]
+                [ div [] [ navbar model.navbar ]
                 , div [ class "flex" ] (List.map viewCourse courses)
                 ]
             ]
@@ -119,12 +129,15 @@ view model =
 
 viewCourse : Course -> Html Msg
 viewCourse course =
-    div [ class "m-2 p-2 shadow" ]
-        [ h1 [ class "text-xl font-semibold tracking-wide leading-loose" ] [ text (course.title ++ " Some") ]
-        , div []
-            [ span [ class "text-sm text-gray-800" ] [ text "Price:" ]
-            , span [ class "font-semibold text-sm text-gray-800" ] [ text "100$" ]
+    div [ class "m-2 p-2 shadow w-1/5 rounded border-l-4 border-blue-300 flex flex-col" ]
+        [ h1 [ class "text-xl font-semibold tracking-wide leading-loose" ] [ text course.title ]
+        , span [ class "text-sm text-gray-800" ] [ text "Price:" ]
+        , span [ class "font-semibold text-sm text-gray-800" ] [ text <| String.fromFloat course.price ]
+        , button
+            [ class "mt-2 p-1 py-2 bg-indigo-700 hover:bg-indigo-500 text-indigo-200 rounded"
+            , onClick <| BuyCourse course.id
             ]
+            [ text "Buy" ]
         ]
 
 
